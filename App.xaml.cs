717a151cb4c3984105a4cf5e61e33e010fbffed9
NZIA <cedricnzia@gmail.com>
2024-03-11 10:19:30 +0100

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CRUDWPFSQLite
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            DatabaseFacade facade=new DatabaseFacade(new DataContext());
+            facade.EnsureCreated();
+        }
+    }
+
+}
---
+++ DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDWPFSQLite
+{
+    public class DataContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = UserData.db");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+
+    
+}
